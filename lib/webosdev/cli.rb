be8353ade2ebd48f2f7d7209a0ab66e92fa8706c
mathieul <mathieul@gmail.com>
2009-07-31 20:00:16 +0200

    added command to generate a new app (just a wrapper around palm-generate)

--- lib/webosdev/cli.rb
+++ lib/webosdev/cli.rb
@@ -2,13 +2,12 @@ require 'optparse'
 
 module Webosdev
   class CLI
+    COMMANDS = %w(new_app new_scene package install)
+
     def self.execute(stdout, arguments=[])
-
-      # NOTE: the option -p/--path= is given as an example, and should be replaced in your application.
-
       options = {
       }
-      mandatory_options = %w(  )
+      mandatory_options = %w(command)
 
       parser = OptionParser.new do |opts|
         opts.banner = <<-BANNER.gsub(/^          /,'')
@@ -20,24 +19,25 @@ module Webosdev
           Options are:
         BANNER
         opts.separator ""
-        opts.on("-a", "--application=NAME", String,
+        opts.on("-a", "--application NAME", String,
                 "Name of the application.") { |name| options[:application] = name }
+        opts.on("-c", "--command COMMAND", String, COMMANDS,
+                "Command to run.") { |command| options[:command] = command }
         opts.on("-h", "--help",
                 "Show this help message.") { stdout.puts opts; exit }
         opts.parse!(arguments)
 
-        if mandatory_options && mandatory_options.find { |option| options[option.to_sym].nil? }
+        if mandatory_options && missing = mandatory_options.find { |option| options[option.to_sym].nil? }
+          STDERR.puts "Please specify the #{missing}.\n\n"
           stdout.puts opts; exit
         end
       end
       
-      tools = PalmTools.new
-
-      application = options[:application] || tools.application_name
-      raise "Unknown application" unless application
-
-      # do stuff
-      stdout.puts "application: #{application.inspect}"
+      manager = PalmManager.new
+      options[:application] ||= manager.application_name
+      raise PalmManager::ArgumentError, "Missing application name" unless options[:application]
+      message = manager.send(options[:command].to_sym, options)
+      stdout.puts "#{message}."
     end
   end
 end
--- lib/webosdev/palm_manager.rb
+++ lib/webosdev/palm_manager.rb
@@ -1,15 +1,17 @@
 %w(haml sass fileutils json).each { |mod| require mod }
 
-class PalmTools
+class PalmManager
   DIRECTORIES = {:haml => %w(. ./app/*/*), :sass => %w(./stylesheets)}
   EXTENSIONS = {:haml => 'html', :sass => 'css'}
   APPINFO_FILE = './appinfo.json'
   
-  class Error < RuntimeError; end
-  class FileNotFoundError < Error; end
+  Error = Class.new(RuntimeError)
+  FileNotFoundError = Class.new(Error)
+  ArgumentError = Class.new(Error)
+  ExecutionError = Class.new(Error)
   
   def files_with_format(extension, options = {})
-    raise "Unknown extension" unless DIRECTORIES[extension]
+    raise ArgumentError, "Unknown extension" unless DIRECTORIES[extension]
     DIRECTORIES[extension].map do |directory|
       files = Dir["#{directory}/*.#{extension}"]
       files.map! {|name| name.sub(/\.#{extension}$/, ".#{EXTENSIONS[extension]}") } if options[:generated]
@@ -34,7 +36,13 @@ class PalmTools
   end
 
   def application_info
-    raise FileNotFoundError, "Can't find file #{APPINFO_FILE.inspect}." unless FileTest.exists?(APPINFO_FILE)
+    raise FileNotFoundError, "Can't find file #{APPINFO_FILE.inspect}" unless FileTest.exists?(APPINFO_FILE)
     @application_info ||= JSON.load(File.read(APPINFO_FILE))
-  end 
+  end
+  
+  def new_app(opts)
+    res = %x(palm-generate #{opts[:application]} 2>&1)
+    raise ExecutionError, res unless $?.success?
+    "New application #{opts[:application].inspect} was created successfully"
+  end
 end
